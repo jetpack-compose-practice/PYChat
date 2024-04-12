6ca5f3a5ca5c477ec42248a0242ec8de92cd24cb
Piyush Prajapati <piyushoa2004@gmail.com>
2024-04-12 17:10:10 +0530

    firebase dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -61,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-firestore:24.11.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -79,4 +81,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.6.5")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
 }
